f1148939f36c92f81ac4764d8a29c42432381d54
Omar Zekri <102541219+Omar50x@users.noreply.github.com>
2022-10-14 18:23:28 +0100

    Add Login And Logout

--- routes/api.php
+++ routes/api.php
@@ -18,10 +18,16 @@ use Illuminate\Support\Facades\Route;
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
 
+Route::middleware(['auth:sanctum', 'IsAPIAdmin'])->group(function () {
+    Route::get('/checkingAuthenticated', function () {
+        return response()->json(['message' => 'You are in', 'status' => 200], 200);
+    });
+});
+
 Route::middleware(['auth:sanctum'])->group(function () {
     Route::post('logout', [AuthController::class, 'logout']);
 });
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
