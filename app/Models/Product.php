2216c8bc78518174b6976f0913444968a651e698
Omar Zekri <102541219+Omar50x@users.noreply.github.com>
2022-10-31 21:00:55 +0100

    Update Product

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Models\Category;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -26,4 +27,9 @@ class Product extends Model
         'popular',
         'status',
     ];
+
+    protected $with = ['category'];
+    public function category() {
+        return $this->belongsTo(Category::class, 'category_id', 'id');
+    }
 }
